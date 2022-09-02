ec92593e767093f75255a20b98873e5e2be1c14c
Ed Locquiao <nilatian.15@gmail.com>
2022-09-01 22:37:16 -0700

    My second push of weeks 8 and 9 and part of week10's assignment

---
+++ mysql-java-recipes/src/main/java/recipes/entity/Unit.java
@@ -0,0 +1,37 @@
+package recipes.entity;
+
+public class Unit {
+	private Integer unitId;
+	private String unitNameSingular;
+	private String unitNamePlural;
+
+	public Integer getUnitId() {
+		return unitId;
+	}
+
+	public void setUnitId(Integer unitId) {
+		this.unitId = unitId;
+	}
+
+	public String getUnitNameSingular() {
+		return unitNameSingular;
+	}
+
+	@Override
+	public String toString() {
+		return "Unit [unitId=" + unitId + ", unitNameSingular=" + unitNameSingular + ", unitNamePlural="
+				+ unitNamePlural + "]";
+	}
+
+	public void setUnitNameSingular(String unitNameSingular) {
+		this.unitNameSingular = unitNameSingular;
+	}
+
+	public String getUnitNamePlural() {
+		return unitNamePlural;
+	}
+
+	public void setUnitNamePlural(String unitNamePlural) {
+		this.unitNamePlural = unitNamePlural;
+	}
+}
